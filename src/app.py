8a7b12d20856a9a8174333e2764314933e33ca7e
Vitor Bueno <84080970+vitorbueno267@users.noreply.github.com>
2025-01-15 00:06:21 -0300

    Piloto

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    title = "Home"
+    return render_template("index.html", title=title)
+
+
+@app.route("/sobremim")
+def sobremim():
+    title = "Sobre mim"
+    return render_template("sobre-mim.html", title=title)
+
+
+@app.route("/projetos")
+def projetos():
+    title = "Projetos"
+    return render_template("projetos.html", title=title)
+
+
+@app.route("/contato")
+def contato():
+    title = "contato"
+    return render_template("contato.html", title=title)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
